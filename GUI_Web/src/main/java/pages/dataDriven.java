704c645c5ca49beebfde7636b82556fdc829fa97
MohabM <mostafa.mohab@vodafone.com>
2024-02-08 15:37:00 +0200

    Add Updates to Main Page Files in main Branch

--- GUI_Web/src/main/java/pages/dataDriven.java
+++ GUI_Web/src/main/java/pages/dataDriven.java
@@ -26,7 +26,7 @@ public class dataDriven {
 		ArrayList<String> array = new ArrayList<String>();
 
 		// file input stream
-		FileInputStream fis = new FileInputStream("C:\\Users\\mosta\\git\\Selenium_E-Commerce_E2E\\excel.xlsx");
+		FileInputStream fis = new FileInputStream("C:\\Users\\MohabM\\IdeaProjects\\ECommerce-SHAFT2\\excel.xlsx");
 
 		@SuppressWarnings("resource")
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
